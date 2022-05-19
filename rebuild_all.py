42e83505c069f933138954db1a048b96e3186ad4
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2022-05-19 13:22:53 +0800

    ok

--- rebuild_all.py
+++ rebuild_all.py
@@ -1,12 +1,12 @@
 import subprocess
 import requests
 
-realeases = requests.get(
+releases = requests.get(
     'https://api.github.com/repos/Anuken/Mindustry/releases').json()
-for index, realease in enumerate(realeases[::-1]):
+for index, realease in enumerate(releases[::-1]):
     tag_name = realease["tag_name"]
-    ext_tag = '\\'
-    if index == len(realeases)-1:
+    ext_tag = ''
+    if index == len(releases) - 1:
         ext_tag += "-t oldshensheep/mindustry-server:latest -t ghcr.io/oldshensheep/mindustry-server:latest \\"
     if realease['prerelease'] == False:
         ext_tag += "-t oldshensheep/mindustry-server:stable -t ghcr.io/oldshensheep/mindustry-server:stable \\"
@@ -16,7 +16,8 @@ for index, realease in enumerate(realeases[::-1]):
     --platform linux/arm,linux/arm64,linux/amd64 \\
     -t oldshensheep/mindustry-server:{tag_name} \\
     -t ghcr.io/oldshensheep/mindustry-server:{tag_name} \\
-    {ext_tag}
+    {ext_tag} \\
     --build-arg "tag_name"="{tag_name}" .
     '''
+    print(call_str)
     subprocess.call(call_str, shell=True)
